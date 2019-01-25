97f62d1f29e538ab87e70c1b219abf24660866d7
Sopss <molinacarlos94@gmail.com>
2019-01-25 08:24:47 +0100

    Añadidas algunas clases y propiedades

--- src/model/seguros/Enfermedades.java
+++ src/model/seguros/Enfermedades.java
@@ -12,5 +12,63 @@ import java.io.Serializable;
  * @author mrsops
  */
 public class Enfermedades implements Serializable{
+    private boolean corazon;
+    private boolean estomacal;
+    private boolean rinyones;
+    private boolean alergia;
+    private String nombreAlergia;
+
+    public Enfermedades(boolean corazon, boolean estomacal, boolean rinyones, boolean alergia, String nombreAlergia) {
+        this.corazon = corazon;
+        this.estomacal = estomacal;
+        this.rinyones = rinyones;
+        this.alergia = alergia;
+        this.nombreAlergia = nombreAlergia;
+    }
+
+    public Enfermedades() {
+    }
+
+    public boolean isCorazon() {
+        return corazon;
+    }
+
+    public void setCorazon(boolean corazon) {
+        this.corazon = corazon;
+    }
+
+    public boolean isEstomacal() {
+        return estomacal;
+    }
+
+    public void setEstomacal(boolean estomacal) {
+        this.estomacal = estomacal;
+    }
+
+    public boolean isRinyones() {
+        return rinyones;
+    }
+
+    public void setRinyones(boolean rinyones) {
+        this.rinyones = rinyones;
+    }
+
+    public boolean isAlergia() {
+        return alergia;
+    }
+
+    public void setAlergia(boolean alergia) {
+        this.alergia = alergia;
+    }
+
+    public String getNombreAlergia() {
+        return nombreAlergia;
+    }
+
+    public void setNombreAlergia(String nombreAlergia) {
+        this.nombreAlergia = nombreAlergia;
+    }
+    
+    
     
 }
